bff3bb66b8aa06ee2bd1468834eb3df2533852b3
Purushothaman Ramasamy <purushothaman.ramasamy@kanini.com>
2021-04-08 11:23:51 +0530

    Added Raining

--- HomeService/Startup.cs
+++ HomeService/Startup.cs
@@ -52,7 +52,7 @@ namespace HomeService
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=GetMobile}/{id?}");
             });
         }
     }
